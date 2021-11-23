ac151dd8f3c214e376c278004959800123df6983
Hermann Vincent <baw.developpement@gmail.com>
2021-11-23 22:09:28 +0400

    initial commit

---
+++ apps/poseidon-front/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'poseidon-front',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/poseidon-front',
+};
