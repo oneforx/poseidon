90d20d61cc4dfd16db9ee3f8233ad33b14bae1ee
Hermann Vincent <baw.developpement@gmail.com>
2021-11-24 00:00:13 +0400

    add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:oneforx/react-translator.git master:gh-pages
+git push -f git@github.com:oneforx/poseidon.git master:gh-pages
 
 cd -
